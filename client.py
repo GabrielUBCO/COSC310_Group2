3469690417319e786098e5f49f1355ba83040089
Yohen Thounaojam <yohenthounaojam@gmail.com>
2020-04-05 00:53:12 -0700

    Adding comments for TA

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 
 x = input("Enter your message.")
-s.send(bytes("Welcome to server", "utf-8"))
+s.send(bytes("Chatting with RamsayBot", "utf-8"))
 
 full_msg=''
 while True:
@@ -13,3 +13,7 @@ while True:
     full_msg += msg.decode("utf-8")
 
 print(full_msg)
+
+
+#This is to be used by the 'other' chatbot to chat with RamsayBot. 
+#The address 1234 must not be changed. 
