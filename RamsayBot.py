0f52393d2369dc4cf3fcd7fc5ec11511ed80db17
Sleepyfatblackcat <61329305+Sleepyfatblackcat@users.noreply.github.com>
2020-04-04 19:59:37 -0700

    Add files via upload

--- RamsayBot.py
+++ RamsayBot.py
@@ -1,12 +1,16 @@
 import nltk
+from nltk.corpus import wordnet
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
+from spellchecker import SpellChecker
 import numpy
 import tensorflow as tf
 import tflearn
 import random
 import json
+import tkinter
+from tkinter import *
 
 with open("topics.json") as file:
     data = json.load(file)
@@ -86,21 +90,66 @@ def bag_of_words(s, words):
 
 
 def chat():
-    print("Hi, I'm RamsayBot \n Type 'quit' to leave ")
-    while True:
-        inp = input("You: ")
-        if inp.lower() == "quit":
-            break
-
-        results = model.predict([bag_of_words(inp, words)])
+    msg = EntryBox.get("1.0",'end-1c').strip()
+    EntryBox.delete("0.0",END)
+    if msg.lower() == "quit":
+        base.destroy()
+        
+    if msg != '':
+        results = model.predict([bag_of_words(msg, words)])
         results_index = numpy.argmax(results)
         tag = labels[results_index]
-
+        
         for tg in data["topics"]:
             if tg['tag'] == tag:
                 responses = tg['responses']
+        
+        ChatLog.config(state=NORMAL)
+        ChatLog.insert(END, "You: " + msg + '\n\n')
+        ChatLog.config(foreground="white", font=("Arial", 12 ))
 
-        print(random.choice(responses))
+        res = random.choice(responses)
+        ChatLog.insert(END, "Gordon: " + res + '\n\n')
+        
+        ChatLog.config(state=DISABLED)
+        ChatLog.yview(END)
 
-chat()
 
+base = Tk()
+base.title("RamsayBot")
+base.geometry("800x450")
+base.resizable(width=FALSE, height=FALSE)
+
+#Create Chat window
+ChatLog = Text(base, bd=0, bg="#404040", height="10", width="50", font="Arial",)
+
+ChatLog.config(state=DISABLED)
+
+#Bind scrollbar to Chat window
+scrollbar = Scrollbar(base, command=ChatLog.yview)
+ChatLog['yscrollcommand'] = scrollbar.set
+
+#Create Button to send message
+SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="12", height=2,
+                    bd=0, bg="#FFB266", activebackground="white",fg='#ffffff',
+                    command= chat )
+
+#Create the box to enter message
+EntryBox = Text(base, bd=0, bg="#E0E0E0",width="29", height="2", font="Arial")
+#EntryBox.bind("<Return>", send)
+
+
+#Place all components on the screen
+scrollbar.place(x=784,y=6, height=386)
+ChatLog.place(x=6,y=6, height=386, width=800)
+EntryBox.place(x=128, y=401, height=30, width=660)
+SendButton.place(x=6, y=401, height=30)
+
+def greet():
+    ChatLog.config(state=NORMAL)
+    ChatLog.insert(END, "Hi, I'm RamsayBot \n Type 'quit' to leave " + '\n\n')
+    ChatLog.config(state=DISABLED)
+    ChatLog.yview(END)
+
+base.after(0, greet)
+base.mainloop()
